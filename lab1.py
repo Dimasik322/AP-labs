cdbd70f017526e02a63271baf4c4c6ceca13c640
Dmitriy <dimakonovalov88@gmail.com>
2023-09-28 16:22:57 +0400

    1.1.1

--- lab1.py
+++ lab1.py
@@ -14,17 +14,19 @@ file = requests.get(URL, headers={"User-Agent":"Mozilla/5.0"}).json()
 date = file['Date']
 value = file['Valute']['USD']['Value']
 previous_URL = file['PreviousURL']
+print("https:"+previous_URL)
 
 with open('dataset.csv', 'w', newline='') as csvfile:
     filewriter = csv.writer(csvfile, delimiter=',',
     quotechar='|', quoting=csv.QUOTE_MINIMAL)
     filewriter.writerow(['Date', 'Value'])
     filewriter.writerow([date[:10], value])
-    for i in range(1000):
-        time.sleep(0.21)
+    for i in range(10000):
+        #time.sleep(0.51)
         URL = 'https:' + previous_URL
         file = requests.get(URL, headers={"User-Agent":"Mozilla/5.0"}).json()
         date = file['Date']
         value = file['Valute']['USD']['Value']
+        print(date[:10], value)
         previous_URL = file['PreviousURL']
         filewriter.writerow([date[:10], value])
