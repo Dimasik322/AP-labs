2ae6c4a647df7c2b00c4bb62bc9490656f5dd292
Dmitriy <dimakonovalov88@gmail.com>
2023-10-29 19:03:09 +0400

    lab2-56

--- lab2/lab2-456/lab2-5.py
+++ lab2/lab2-456/lab2-5.py
@@ -1,2 +1,23 @@
+import csv
+
+i = 1
+
 def next():
-    if 
+    global i
+    if i <= 7186:
+        with open('lab1/dataset.csv', "r") as file:
+            reader = csv.reader(file, delimiter=",")
+            j = 0
+            for row in reader:
+                if i == j:
+                    x = row
+                    break
+                j+=1
+            file.close()
+        i+=1
+        return row
+        
+print(next())
+print(next())
+print(next())
+print(next())
--- lab2/lab2-456/lab2-6.py
+++ lab2/lab2-456/lab2-6.py
@@ -1,16 +1,25 @@
+import csv
+
 class SimpleIterator:
-    def __init__(self, limit):
-        self.limit = limit
-        self.counter = 0
+    def __init__(self):
+        self.limit = 7187
+        self.counter = 1
 
     def __iter__(self):
         return self
     
     def __next__(self):
         if self.counter < self.limit:
-            self.counter +=1
-            return 1
-        
-s_iter = SimpleIterator(5)
-for val in s_iter:
-    print(val)
+            with open('lab1/dataset.csv', "r") as file:
+                reader = csv.reader(file, delimiter=",")
+                j = 0
+                for row in reader:
+                    if self.counter == j:
+                        x = row
+                        break
+                    j+=1
+                file.close()
+            self.counter+=1
+            return row
+        else:
+            raise StopIteration
